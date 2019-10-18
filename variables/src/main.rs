ee20fc3a1dbc2dae03b68b2edb9f61c755a5a84f
Sarah McCuan <sarahmccuan1@gmail.com>
2019-10-18 17:44:01 -0400

    fooling around with variables

---
+++ variables/src/main.rs
@@ -0,0 +1,8 @@
+//This fails 
+fn main() {
+    let x = 5;
+    println!("The value of x is {}", x);
+    x = 6;
+    println!("The value of x is {}", x);
+}
+
---
+++ variables/target/debug/deps/variables-80742407c823f7d9.d
@@ -0,0 +1,5 @@
+/home/sarah/RustScratchWork/variables/target/debug/deps/variables-80742407c823f7d9: src/main.rs
+
+/home/sarah/RustScratchWork/variables/target/debug/deps/variables-80742407c823f7d9.d: src/main.rs
+
+src/main.rs:
