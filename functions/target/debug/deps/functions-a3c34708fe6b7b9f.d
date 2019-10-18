b951331919d1563d068405fc9885e7f38ada0097
Sarah McCuan <sarahmccuan1@gmail.com>
2019-10-18 18:12:17 -0400

    playing around with functions

---
+++ functions/target/debug/deps/functions-a3c34708fe6b7b9f.d
@@ -0,0 +1,5 @@
+/home/sarah/RustScratchWork/functions/target/debug/deps/functions-a3c34708fe6b7b9f: src/main.rs
+
+/home/sarah/RustScratchWork/functions/target/debug/deps/functions-a3c34708fe6b7b9f.d: src/main.rs
+
+src/main.rs:
---
+++ functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+/home/sarah/RustScratchWork/functions/target/debug/functions: /home/sarah/RustScratchWork/functions/src/main.rs
