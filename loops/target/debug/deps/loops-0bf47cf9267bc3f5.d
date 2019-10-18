6bd4c015083d2b033ce5695355c4f376e6800cb1
Sarah McCuan <sarahmccuan1@gmail.com>
2019-10-18 18:50:45 -0400

    returning a value from a loop is easy

---
+++ loops/target/debug/deps/loops-0bf47cf9267bc3f5.d
@@ -0,0 +1,5 @@
+/home/sarah/RustScratchWork/loops/target/debug/deps/loops-0bf47cf9267bc3f5: src/main.rs
+
+/home/sarah/RustScratchWork/loops/target/debug/deps/loops-0bf47cf9267bc3f5.d: src/main.rs
+
+src/main.rs:
---
+++ loops/target/debug/loops.d
@@ -0,0 +1,1 @@
+/home/sarah/RustScratchWork/loops/target/debug/loops: /home/sarah/RustScratchWork/loops/src/main.rs
