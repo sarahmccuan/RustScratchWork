863f469a8c7fa9e0ee53b1fe362f8843c99292ad
Sarah McCuan <sarahmccuan1@gmail.com>
2019-10-18 18:59:07 -0400

    using for instead of while for countdown

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
-    let a = [10, 20, 30, 40, 50]; // how to explicitly define types?
-    
-    for element in a.iter() {
-        println!("The value is {}", element);
+    for number in (1..4).rev() {
+        println!("{}!", number);
     }
+    println!("LIFTOFF");
 }
+
