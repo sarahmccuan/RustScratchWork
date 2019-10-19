b5bf0a630b15ca2a21432b8b40616fbcd6709e0f
Sarah McCuan <sarahmccuan1@gmail.com>
2019-10-19 13:50:01 -0400

    temp conversion working except for exit condition

--- temp_conversion/src/main.rs
+++ temp_conversion/src/main.rs
@@ -1,21 +1,27 @@
 use std::io;
 
 fn main() {
-    let mut user_input = String::new();
+    loop {
+        println!("Please input a temperature in Fahrenheit (or \"exit\" to continue): ");
 
-    loop { // I need to have expect here somehow
-        user_input = io::stdin().read_line(&mut user_input)
-            .unwrap();
-        if user_input == "exit" {
-            break;
-        } else {
-            println!("User input is {}", user_input);
-        }
+        let mut fahr_input = String::new();
+        io::stdin().read_line(&mut fahr_input)
+            .expect("Failed to read line");
+        
+        let fahr_input: f64 = match fahr_input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Not a number, please enter a number.");
+                continue;
+            }
+        };
+
+        println!("Value in Celsius is {}", convert_fahr_to_celsius(fahr_input as f64));
     }
     
-    println!("Swapped value is {}", convert_fahr_to_celsius(1.1));
 }
 
 fn convert_fahr_to_celsius(fahrenheit: f64) -> f64 {
-    5.0
+    let celsius = (5.0/9.0) * (fahrenheit - 32.0);
+    celsius
 }
---
+++ temp_conversion/target/debug/temp_conversion.d
@@ -0,0 +1,1 @@
+/home/sarah/RustScratchWork/temp_conversion/target/debug/temp_conversion: /home/sarah/RustScratchWork/temp_conversion/src/main.rs
